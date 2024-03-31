ba35037a19e061ac9fe399fbc872f4aa11104696
RIATRAIL <165512304+RIATRAIL@users.noreply.github.com>
2024-03-31 12:23:23 +0200

    Update streamlit_app.py
    add import pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 
 # Write directly to the app
@@ -21,8 +22,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #get_active_session()
 
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark Datafram to a Pandas Dataframe so we can use the LOC function
+pd_df = my_datafram.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list =options = st.multiselect(
@@ -38,7 +45,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-
+        
+        # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
