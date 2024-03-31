182770fb42078a40181c6311811f808b6f61518f
RIATRAIL <165512304+RIATRAIL@users.noreply.github.com>
2024-03-31 14:44:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 # Convert the Snowpark Datafram to a Pandas Dataframe so we can use the LOC function
