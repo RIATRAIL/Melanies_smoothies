6e01046eb164f478d18f94ac5720ffd22803805f
RIATRAIL <165512304+RIATRAIL@users.noreply.github.com>
2024-03-31 14:39:50 +0200

    Update streamlit_app.py
    use search_on value

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,15 +45,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string +=fruit_chosen + ' '
-            
+
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                    
              # New section to display fruityvice nutrition information 
             st.subheader(fruit_chosen + ' Nutrion Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-            # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
+            
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
