66bfe18700165cac50ef4c439bbc8aed0cf5005a
RIATRAIL <165512304+RIATRAIL@users.noreply.github.com>
2024-03-31 14:57:20 +0200

    Update streamlit_app.py
    Making Use of the SEARCH_ON Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 # Convert the Snowpark Datafram to a Pandas Dataframe so we can use the LOC function
@@ -47,11 +47,11 @@ if ingredients_list:
             ingredients_string +=fruit_chosen + ' '
 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
                     
              # New section to display fruityvice nutrition information 
             st.subheader(fruit_chosen + ' Nutrion Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
             
